4177862307a550f7fc25a568a5586a4d7df2f972
Satayu <52563333+u6288016@users.noreply.github.com>
2022-04-28 10:58:02 +0700

    Add files via upload

--- delivery/ios/Runner/AppDelegate.swift
+++ delivery/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMap
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBudcroV57gkbHHyxMB0w3mh_EF2JKRzhY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
